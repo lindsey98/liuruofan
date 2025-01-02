85f5bd742d6d677219fb56c971dbc491270ce82e
lindsey98 <51521323+lindsey98@users.noreply.github.com>
2025-01-02 11:42:27 +0800

    Update _main.js

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -1,9 +1,5 @@
-/* ==========================================================================
-   jQuery plugin settings and other scripts
-   ========================================================================== */
-
-$(document).ready(function(){
-   // Sticky footer
+$(document).ready(function() {
+  // Sticky Footer
   var bumpIt = function() {
       $("body").css("margin-bottom", $(".page__footer").outerHeight(true));
     },
@@ -20,79 +16,60 @@ $(document).ready(function(){
       bumpIt();
     }
   }, 250);
+
   // FitVids init
   $("#main").fitVids();
 
-  // init sticky sidebar
+  // Sticky Sidebar Initialization
   $(".sticky").Stickyfill();
 
-  var stickySideBar = function(){
+  var stickySideBar = function() {
     var show = $(".author__urls-wrapper button").length === 0 ? $(window).width() > 1024 : !$(".author__urls-wrapper button").is(":visible");
-    // console.log("has button: " + $(".author__urls-wrapper button").length === 0);
-    // console.log("Window Width: " + windowWidth);
-    // console.log("show: " + show);
-    //old code was if($(window).width() > 1024)
     if (show) {
-      // fix
       Stickyfill.rebuild();
       Stickyfill.init();
       $(".author__urls").show();
     } else {
-      // unfix
       Stickyfill.stop();
       $(".author__urls").hide();
     }
   };
 
+  // Adjust Sidebar Width Dynamically
+  var adjustSidebarWidth = function() {
+    if ($(window).width() > 1024) {
+      $(".author__urls-wrapper").css({
+        "width": "300px",
+        "max-width": "300px"
+      });
+    } else {
+      $(".author__urls-wrapper").css({
+        "width": "100%"
+      });
+    }
+  };
+
   stickySideBar();
+  adjustSidebarWidth();
 
-  $(window).resize(function(){
+  $(window).resize(function() {
     stickySideBar();
+    adjustSidebarWidth();
   });
 
-  // Follow menu drop down
+  // Smooth Scroll
+  $("a").smoothScroll({ offset: -20 });
 
-  $(".author__urls-wrapper button").on("click", function() {
-    $(".author__urls").fadeToggle("fast", function() {});
-    $(".author__urls-wrapper button").toggleClass("open");
-  });
-
-  // init smooth scroll
-  $("a").smoothScroll({offset: -20});
-
-  // add lightbox class to all image links
-  $("a[href$='.jpg'],a[href$='.jpeg'],a[href$='.JPG'],a[href$='.png'],a[href$='.gif']").addClass("image-popup");
-
-  // Magnific-Popup options
+  // Lightbox for Images
   $(".image-popup").magnificPopup({
-    // disableOn: function() {
-    //   if( $(window).width() < 500 ) {
-    //     return false;
-    //   }
-    //   return true;
-    // },
-    type: 'image',
-    tLoading: 'Loading image #%curr%...',
+    type: "image",
     gallery: {
-      enabled: true,
-      navigateByImgClick: true,
-      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+      enabled: true
     },
     image: {
-      tError: '<a href="%url%">Image #%curr%</a> could not be loaded.',
+      tError: "<a href='%url%'>Image #%curr%</a> could not be loaded."
     },
-    removalDelay: 500, // Delay in milliseconds before popup is removed
-    // Class that is added to body when popup is open.
-    // make it unique to apply your CSS animations just to this exact popup
-    mainClass: 'mfp-zoom-in',
-    callbacks: {
-      beforeOpen: function() {
-        // just a hack that adds mfp-anim class to markup
-        this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
-      }
-    },
-    closeOnContentClick: true,
-    midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+    mainClass: "mfp-zoom-in",
+    midClick: true
   });
-
 });
